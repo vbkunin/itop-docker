6e9cd798752aa7c1260934da3ffe00f2cc6a5e6e
Vladimir Kunin <v.b.kunin@gmail.com>
2017-08-29 20:37:29 +0300

    wtf

--- artifacts/make-itop-config-writable.sh
+++ artifacts/make-itop-config-writable.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-chmod -f u+w /app/conf/production/config-itop.php
+chmod -f 644 /app/conf/production/config-itop.php
 echo "iTop config is writable now. Start update wizard http://localhost/setup in your browser."

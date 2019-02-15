e5a2817f67cf97241dc4ba7b33cf80f90f00475e
Kai <kboenke@users.noreply.github.com>
2019-02-15 17:54:10 -0500

    Check if $no_database is set to skip MySQL Init

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@
 
 VOLUME_HOME="/var/lib/mysql"
 
-if [[ ! -d $VOLUME_HOME/mysql ]]; then
+if [ $no_database == true ]; then
+    echo "=> Using external MySQL server"
+elif [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
     echo "=> Installing MySQL ..."
     mysql_install_db > /dev/null 2>&1

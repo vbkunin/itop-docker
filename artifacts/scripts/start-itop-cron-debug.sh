2c4e3aec8b46f78470a073243d1f26ec236810a4
Vladimir Kunin <v.b.kunin@gmail.com>
2019-02-21 00:38:27 +0300

    Updated debug scripts

--- artifacts/scripts/start-itop-cron-debug.sh
+++ artifacts/scripts/start-itop-cron-debug.sh
@@ -2,6 +2,6 @@
 
 DEFAULT_SERVER_NAME=localhost
 DEFAULT_AUTH_USER=admin
-DEFAULT_AUTH_PWD=
+DEFAULT_AUTH_PWD=password
 
-PHP_IDE_CONFIG="serverName=${1:-$DEFAULT_SERVER_NAME}" php /var/www/html/webservices/cron.php --auth_user=${2:-$DEFAULT_AUTH_USER} --auth_pwd=${3:-$DEFAULT_AUTH_PWD} --verbose=1 --debug=1
+PHP_IDE_CONFIG="serverName=${3:-$DEFAULT_SERVER_NAME}" php /var/www/html/webservices/cron.php --auth_user=${1:-$DEFAULT_AUTH_USER} --auth_pwd=${2:-$DEFAULT_AUTH_PWD} --verbose=1 --debug=1

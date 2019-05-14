4621b3c27991df7be36fdadaa4b275faa7152959
Vladimir Kunin <v.b.kunin@gmail.com>
2019-05-14 21:52:16 +0300

    curl instead of wget in toolkit installation script

--- artifacts/scripts/install-toolkit.sh
+++ artifacts/scripts/install-toolkit.sh
@@ -5,7 +5,7 @@ TOOLKIT_FOLDER=$APP_PATH/toolkit
 
 if [[ -d $APP_PATH ]]; then
     rm -rf $TOOLKIT_FOLDER
-    wget -O /tmp/toolkit.zip $TOOLKIT_URL
+    curl -SL -o /tmp/toolkit.zip $TOOLKIT_URL
     unzip /tmp/toolkit.zip -d /tmp/
     cp -r /tmp/toolkit $APP_PATH && rm -rf /tmp/toolkit*
     echo "Go to http://localhost/toolkit to continue installation."

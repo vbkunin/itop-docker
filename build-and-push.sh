05d5a20a56dee80fdd1122700ea855aa18da296b
Vladimir Kunin <v.b.kunin@gmail.com>
2024-09-29 17:59:50 +0300

    Toolkit 3.2

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 # Example:
-#  BUILD_TARGET=base
-#  IMAGE_TAG=3.1.0-base
-#  ITOP_DOWNLOAD_URL=https://sourceforge.net/projects/itop/files/itop/3.1.1-1/iTop-3.1.1-1-12561.zip/download
+# BUILD_TARGET=base
+# IMAGE_TAG=3.2.0-base
+# ITOP_DOWNLOAD_URL=https://sourceforge.net/projects/itop/files/itop/3.2.0-2/iTop-3.2.0-2-14758.zip/download
 #
 # Create and use a new builder instance (if needed):
 #  docker buildx create --name container --driver docker-container --bootstrap --use
